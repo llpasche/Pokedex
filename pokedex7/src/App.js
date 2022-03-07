4138dd0be5b78c8a3f162316aa6f34b1092b0b10
Lucas Pasche <paschelucas9@gmail.com>
2022-03-07 17:33:16 -0300

    header feito

--- pokedex7/src/App.js
+++ pokedex7/src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
+import "./App.css"
+import Header from "./components/header/Header";
+import { createGlobalStyle } from 'styled-components';
+import { ThemeProvider } from "@emotion/react";
+import theme from "./constants/theme";
+ 
+// Estilos definidos aqui serão aplicados a toda a aplicação
+const GlobalStyle = createGlobalStyle`
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 
 const App = () => {
   return (
+    <ThemeProvider theme={theme}>
     <div>
+      <GlobalStyle/>
+      
+      <Header />
       <h1>App</h1>
+      
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ pokedex7/src/constants/colors.js
@@ -0,0 +1,4 @@
+export const primaryColor = "#EF3E36";
+export const secondaryColor = "#17BEBB";
+export const terciaryColor = "#2E282A";
+export const neutralColor = "#FAD8D6";
