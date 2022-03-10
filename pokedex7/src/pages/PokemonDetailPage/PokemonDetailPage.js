d46a8fa3740288fd54d693411dfff27cf259143c
enzopasche <Enzo Pasche>
2022-03-10 16:33:59 -0300

    Stats, sprites e moves com erro

--- pokedex7/src/pages/PokemonDetailPage/PokemonDetailPage.js
+++ pokedex7/src/pages/PokemonDetailPage/PokemonDetailPage.js
@@ -1,3 +1,43 @@
+import { useParams } from "react-router-dom"
+import { BASE_URL } from "../../constants/url"
+import { useRequestData5 } from "../../hooks/useRequestData"
+import Sprites from "../../components/Sprites/Sprites"
+import Stats from "../../components/Stats/Stats"
+import { Container } from "./styled"
+
+
+
 export default function PokemonDetailPage() {
-  return <div>PokemonDetailPage</div>;
+  
+  const params = useParams()
+  console.log(params)
+  
+  const pokemonMoves = useRequestData5([], `${BASE_URL}/pokemon/${params.id}/`)
+  console.log(pokemonMoves.moves)
+
+  const pokemon = pokemonMoves.map(move =>{
+    return(
+      <div key={move.moves.move}>
+        <p>{move.moves.move}: {move.moves.move}</p>
+      </div>
+    )
+  })
+  
+
+  
+  return(
+    <div>
+      
+      <button>Voltar</button>
+      <button>Adicionar/Remover da Pokédex</button>
+      <Container>
+        <Sprites />
+        <Stats />
+      </Container>
+      
+      <p>Tipo</p>
+      <p>Ataques</p>
+      <p>Atributos</p>
+    </div>
+  ) 
 }
